e8080b9619e3a925f582699ffecdecde4d48b82b
Wendy <wendyc8035@queenstechhs.org>
2018-11-29 23:02:48 +0000

    11/29

--- js/script.js
+++ js/script.js
@@ -9,10 +9,42 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+//var userchoice = "";
+//var computerChoice = "";
+//var winner = "";
+//var randomNumber = 0;
+
+//var choices = ["rock", "paper", "scissors"];
+
 $("button").click(function() {
-var message = $("#input").val();
-$("#userChoice").text(message);
+var userchoice = $("#input").val();
+$("#userChoice").text(userchoice);
+
+var computerChoice = Math.floor(Math.random() * 3);
+
+
+if (computerChoice === 0){
+    $("#computerChoice").text("rock");
+} else if (computerChoice === 1){
+     $("#computerChoice").text("paper");
+} else if (computerChoice === 2){
+     $("#computerChoice").text("scissors");
+}
+
+      if(userchoice === computerChoice){
+          $("#result").text("Tie");
+      } else if(userchoice === "rock" && computerChoice === "paper"){
+          $("#result").text("YOU LOSE!");
+      }else if(userchoice === "rock" && computerChoice === "scissors"){
+          $("#result").text("YOU WIN!");
+      }else if(userchoice === "paper" && computerChoice === "rock"){
+          $("#result").text("YOU WIN!");
+      }else if(userchoice === "paper" && computerChoice === "scissors"){
+          $("#result").text("YOU LOSE!");
+      }else if(userchoice === "scissors" && computerChoice === "rock"){
+          $("#result").text("YOU LOSE!");
+      }else if(userchoice === "scissors" && computerChoice === "paper"){
+         $("#result").text("YOU WIN!");
+      }
+      
 });
-
-
-
